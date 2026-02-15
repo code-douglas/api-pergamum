659a132a80edf9d958cf7583163366c5db1a9b07
Douglas <douglasd3v.contato@gmail.com>
2026-02-15 17:41:15 -0300

    feat: configure global middlewares for security, cors and logging

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
+import cors from 'cors';
 import express from 'express';
+import hemlet from 'helmet';
+import morgan from 'morgan';
+
 const app = express();
+
+app.use(cors());
+app.use(hemlet());
+app.use(morgan('dev'));
+app.use(express.json());
 
 export default app;
